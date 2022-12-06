538267f4cf3e999dfeb40268a803ff1ae79522c3
Duc <hdrake1302@gmail.com>
2022-12-06 11:59:16 +0700

    finish append & tdtu elearning

---
+++ scripts/tdtu-elearning/tdtu-elearning-complete.js
@@ -0,0 +1,26 @@
+async function main() {
+  const questions = document.querySelectorAll(".multichoice");
+
+  const keys = {};
+  for (const questionEle of questions) {
+    const question = questionEle.querySelector(".qtext").innerText.trim();
+    const input = questionEle.querySelector('[checked="checked"]');
+
+    const answerEle = input.closest("div");
+
+    if (question && answerEle) {
+      const answer = answerEle.querySelector("label").innerText.trim();
+
+      keys[question] = answer.slice(3);
+    }
+  }
+
+  sendMessage({
+    type: "save-keys",
+    data: {
+      keys,
+    },
+  });
+}
+
+main();
